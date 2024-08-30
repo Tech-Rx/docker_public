376f8e2223931d237a52a79e84983eb34cd4620f
absaro <119333507+absaro@users.noreply.github.com>
2024-08-31 01:32:14 +0500

    Update setup_repocket.sh

--- setup_repocket.sh
+++ setup_repocket.sh
@@ -5,6 +5,6 @@ docker pull repocket/repocket:latest
 
 # Run the Repocket container with environment variables directly
 docker run --name repocket \
-  -e RP_EMAIL=absaralam.crypto@gmail.com \
-  -e RP_API_KEY=cdce0bcb-722b-4c03-8c96-938d583ad85c \
+  -e RP_EMAIL=example@gmail.com \
+  -e RP_API_KEY=(your_api_key) \
   -d --restart=always repocket/repocket
